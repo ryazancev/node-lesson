20c59cc39316b2bbc2e531fa435e26b104b53785
Рязанцев Евгений <ryazancev.e@gmail.com>
2021-09-15 19:00:52 +0300

    block_3_send-form

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-// const path = require('path');
 const app = express();
+const homeRoutes = require('./routes/home.js');
+const addRoutes = require('./routes/add.js');
+const coursesRoutes = require('./routes/courses.js');
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 });
 
-
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
-app.get('/', (req, res) => {
-    res.status(200)
-    res.render('index');
-});
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
 
-app.get('/about', (req, res) => {
-    res.status(200);
-    res.render('about');
-});
-
-
-
-
-
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
 const PORT = process.env.PORT || 3000;
 
